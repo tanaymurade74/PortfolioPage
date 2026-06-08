e247d01e42c5168e357c010d65f8333cf17967f3
tanaymurade74 <tanaymurade8@gmail.com>
2026-06-08 08:39:43 +0530

    Update Google Drive link in App.js

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
             Contact Me
           </a>
           <a
-            href="https://drive.google.com/file/d/1sdcoJx6fIJL_btQAMn54y8H41jRneRlr/view?usp=sharing"
+            href="https://drive.google.com/file/d/1TjMX9iiZH9PQYtwGZY4HSai5eE4-qsGe/view?usp=drive_link"
             rel="noreferrer"
             target="_blank"
             className="btn btn-outline-light ms-3 px-4 py-2"
